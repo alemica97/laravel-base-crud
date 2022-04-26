75e0289ee59824ebe68b4c49c1238b3f0a8d5640
alemica97 <alessandro.micalizzi08@gmail.com>
2022-04-26 16:32:30 +0200

    tramite il metodo resource ho creato le rotte necessarie per Comic

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Creo le rotte necessarie utilizzando il metodo 'resource'
+Route::resource('comics','ComicController');
